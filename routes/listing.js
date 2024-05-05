89c6661267f337dce2209df3534a79dc439d4603
Abhinandan-Sah <abhinandansah222@gmail.com>
2024-05-05 22:50:35 +0530

    updated

---
+++ routes/listing.js
@@ -0,0 +1,94 @@
+const express = require("express");
+const router = express.Router();
+const wrapAsync = require("../utils/wrapAsync.js");
+const ExpressError = require("../utils/ExpressError.js");
+const {listingSchema, reviewSchema} = require("../schema.js");
+const Listing = require("../models/listing.js");
+
+
+const validateListing = (req, res, next) => {
+  let { error } = listingSchema.validate(req.body);
+  if (error) {
+    let errMsg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(400, errMsg);
+  } else {
+    next();
+  }
+};
+
+//Index Route
+router.get("/", async (req, res) => {
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs", { allListings });
+  });
+
+//New Route
+router.get("/new", (req, res) => {
+  res.render("listings/new.ejs");
+});
+
+//Show Route
+router.get(
+    "/:id",
+    wrapAsync(async (req, res, next) => {
+      const { id } = req.params;
+      const listing = await Listing.findById(id).populate("reviews");
+      res.render("listings/show.ejs", { listing });
+    })
+  );
+
+//Create Route and error handling done on it with wrapAsync
+router.post(
+  "/",
+  wrapAsync(async (req, res, next) => {
+    let newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+  })
+);
+
+//Edit Route
+router.get(
+  "/:id/edit",
+  wrapAsync(async (req, res, next) => {
+    const { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs", { listing });
+  })
+);
+
+//Update Route
+router.put(
+  "/:id",
+  wrapAsync(async (req, res, next) => {
+    let { id } = req.params;
+    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+    res.redirect(`/listings/${id}`);
+  })
+);
+
+//Delete Route
+router.delete(
+  "/:id",
+  wrapAsync(async (req, res, next) => {
+    let { id } = req.params;
+    let deletedListing = await Listing.findByIdAndDelete(id);
+    res.redirect("/listings");
+  })
+);
+
+
+
+
+
+router.all("*", (req, res, next) => {
+  next(new ExpressError(404, "Page Not Found!"));
+});
+
+//middleware for error handling
+router.use((err, req, res, next) => {
+  let { statusCode = 500, message = "Something went wrong!" } = err;
+  res.status(statusCode).render("error.ejs", { message });
+});
+
+module.exports = router;
