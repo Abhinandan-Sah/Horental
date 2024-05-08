7917ac3646b05b3ce00b37c5f33ecbab47fbe02a
Abhinandan-Sah <abhinandansah222@gmail.com>
2024-05-08 22:49:50 +0530

    update

--- app.js
+++ app.js
@@ -70,7 +70,7 @@ app.all("*", (req, res, next)=> {
 app.use((err, req, res, next) => {
   let {statusCode=500, message = "Something went wrong!" } = err;
   res.status(statusCode).render("error.ejs", {message});
-  // res.status(statusCode).send(message);
+
 })
 
 app.listen(8080, () => {
