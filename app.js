db5a3466f3d342d3fed3681b49a1544e460e4160
Abhinandan-Sah <abhinandansah222@gmail.com>
2024-05-14 18:47:41 +0530

    updated

--- app.js
+++ app.js
@@ -26,7 +26,13 @@ app.use(express.static(path.join(__dirname, "/public")));
 const sessionOptions = {
   secret: "mysupersecretcode",
   resave: false,
-  saveUninitialized: true
+  saveUninitialized: true,
+  cookie: {
+    expires: Date.now() + 7*24*60*60*1000,
+    maxAge: 7*24*60*60*1000,
+    httpOnly:true,
+  }
+
 }
 
 app.use(session(sessionOptions));
